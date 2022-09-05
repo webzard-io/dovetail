d3883550e40b4b3fdde66feb6f019d8dedce39eb
MrWindlike <MrWindlike@gmail.com>
2022-09-05 11:21:46 +0800

    fix(Sync): only sync form value to the component

--- src/sunmao/traits/SyncKubectlApplyForm.tsx
+++ src/sunmao/traits/SyncKubectlApplyForm.tsx
@@ -3,17 +3,11 @@ import { implementRuntimeTrait } from "@sunmao-ui/runtime";
 import get from "lodash/get";
 
 const TransformerTraitPropertiesSpec = Type.Object({
-  value: Type.Any({
-    title: "Value",
-  }),
   setValueMethod: Type.String({
     title: "Set Value Method",
   }),
-  formId: Type.String({
-    title: "Form ID",
-  }),
-  fieldPath: Type.String({
-    title: "Field Path",
+  formValue: Type.String({
+    title: "Form value",
   }),
 });
 const TransformerTraitStateSpec = Type.Object({});
@@ -38,61 +32,56 @@ export default implementRuntimeTrait({
   },
 })(() => {
   const hasInitializedMap = new Map<string, boolean>();
-  const valueCache = new Map<string, any>();
+  const formItemValueCache = new Map<string, any>();
+  const updateMap = new Map<string, boolean>();
+
   return ({
-    value,
-    formId,
-    fieldPath,
+    formValue,
+    setValueMethod,
     services,
     componentId,
     subscribeMethods,
-    setValueMethod,
   }) => {
-    const cachedValue = valueCache.get(componentId);
-    const hasInitialized = hasInitializedMap.get(componentId);
+    function syncToComponent() {
+      const cachedFormItemValue = formItemValueCache.get(componentId);
 
-    valueCache.set(componentId, value);
-
-    if (!hasInitialized) {
-      hasInitializedMap.set(componentId, true);
-      subscribeMethods({
-        syncToComponent({ value: formValue }: any) {
-          const valueInForm = get(formValue, fieldPath);
-          const currentValue = valueCache.get(componentId);
-          if (currentValue !== valueInForm) {
-            services.apiService.send("uiMethod", {
-              componentId,
-              name: setValueMethod,
-              parameters: valueInForm,
-            });
-          }
-        },
-      });
-    }
-
-    function setField() {
-      if (value !== cachedValue) {
+      if (formValue !== cachedFormItemValue) {
         services.apiService.send("uiMethod", {
-          componentId: formId,
-          name: "setField",
-          parameters: {
-            fieldPath,
-            value,
-          },
+          componentId,
+          name: setValueMethod,
+          parameters: formValue,
         });
       }
+
+      formItemValueCache.set(componentId, formValue);
+    }
+
+    if (hasInitializedMap.has(componentId) === false) {
+      services.apiService.on(
+        "uiMethod",
+        ({ componentId: target, name: event, parameters }) => {
+          if (target === componentId && event === "onChange") {
+            updateMap.set(componentId, true);
+          }
+        }
+      );
     }
 
     return {
       props: {
         componentDidMount: [
           () => {
-            setField();
+            syncToComponent();
           },
         ],
         componentDidUpdate: [
           () => {
-            setField();
+            syncToComponent();
+          },
+        ],
+        componentDidUnmount: [
+          () => {
+            formItemValueCache.delete(componentId);
           },
         ],
       },
