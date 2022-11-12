1074bf09ef4a567241d445889777f178845d4670
MrWindlike <MrWindlike@gmail.com>
2022-11-12 14:14:17 +0800

    fix(KGD): change the item key

--- src/_internal/organisms/KubectlGetDetail/KubectlGetDetail.tsx
+++ src/_internal/organisms/KubectlGetDetail/KubectlGetDetail.tsx
@@ -252,7 +252,7 @@ const KubectlGetDetail = React.forwardRef<
                       };
                       return (
                         <kit.InfoRow
-                          key={item.path || item.label}
+                          key={(item.key || item.path || item.label) + index}
                           className={RowStyle}
                           label={
                             <span className={Typo.Label.l4_regular_title}>
