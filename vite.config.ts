aea95543057dfb32fb1a472327f76480d69331a0
Bowen Tan <bowen.tan@smartx.com>
2023-01-31 11:27:27 +0800

    build commonjs version

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "Kui",
       fileName: (format) => `index.${format}.js`,
-      formats: ["es"],
+      formats: ["es", "cjs"],
     },
     rollupOptions: {
       external: [
