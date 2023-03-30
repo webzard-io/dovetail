d2d31e960020b2618375349f989762d3c9f09d8b
MrWindlike <MrWindlike@gmail.com>
2023-03-30 13:56:05 +0800

    feat: export i18nInstance

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { libs as K8sLib } from "./sunmao/lib";
 import "./locales";
+import i18nInstance from "./i18n";
 import registry from "./services/Registry";
 
-export { KubeApi } from "./_internal/k8s-api-client/kube-api"
+export { KubeApi } from "./_internal/k8s-api-client/kube-api";
 export { K8sLib };
 export { registry };
+export { i18nInstance };
