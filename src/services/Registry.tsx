d6338001f39a5d269f0f474c3e9b311330387bb8
MrWindlike <MrWindlike@gmail.com>
2023-04-10 13:53:59 +0800

    feat(KAF): add some validation rules of k8s name

--- src/services/Registry.tsx
+++ src/services/Registry.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { RuleItem } from "async-validator";
-import { ipv4, email, url } from "../sunmao/utils/validators";
+import { ipv4, email, url, dnsSubdomain, rfc1035, rfc1123 } from "../sunmao/utils/validators";
 
 class Registry {
   public icons: Map<string, React.ReactNode> = new Map();
@@ -28,5 +28,8 @@ const registry = new Registry();
 registry.registerValidator("ipv4", ipv4);
 registry.registerValidator("email", email);
 registry.registerValidator("url", url);
+registry.registerValidator("dnsSubdomain", dnsSubdomain);
+registry.registerValidator("rfc1035", rfc1035);
+registry.registerValidator("rfc1123", rfc1123);
 
 export default registry;
