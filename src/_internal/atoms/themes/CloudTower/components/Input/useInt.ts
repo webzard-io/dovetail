c1862d1164f4eb24ef121e1b14b952f93220e484
MrWindlike <MrWindlike@gmail.com>
2024-02-18 10:48:05 +0800

    fix(InputNumber): handle the big number

--- src/_internal/atoms/themes/CloudTower/components/Input/useInt.ts
+++ src/_internal/atoms/themes/CloudTower/components/Input/useInt.ts
@@ -5,7 +5,7 @@ type Props = {
   supportNegativeValue?: boolean;
   maximum?: number;
   minimum?: number;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>, value: string | number) => void;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>, value: string | number, rawValue: string) => void;
   onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
 };
 
@@ -15,14 +15,14 @@ function useInt(props: Props) {
       const value = e.currentTarget.value;
       if (props.supportNegativeValue) {
         if (value === "" || value === "-") {
-          props.onChange?.(e, value);
+          props.onChange?.(e, value, value);
         } else if (/^(-)?\d+$/.test(value)) {
           const v = parseInt(value);
-          props.onChange?.(e, !Number.isNaN(v) ? v : '');
+          props.onChange?.(e, !Number.isNaN(v) ? v : "", value);
         }
       } else if (value === "" || /^\d+$/.test(value)) {
         const v = parseInt(value);
-        props.onChange?.(e, !Number.isNaN(v) ? v : '');
+        props.onChange?.(e, !Number.isNaN(v) ? v : "", value);
       }
     },
     [props.onChange, props.supportNegativeValue]
@@ -33,16 +33,17 @@ function useInt(props: Props) {
       const minimumIsValid = typeof props.minimum === "number";
 
       if (maximumIsValid || minimumIsValid) {
-        const value = parseInt(e.target.value) || 0;
+        const rawValue = e.target.value;
+        const value = parseInt(rawValue) || 0;
         
         if (isNil(value)) {
-          props.onChange?.(e, '');
+          props.onChange?.(e, "", rawValue);
         }
         if (!isNil(value) && maximumIsValid && (props.maximum || 0) < value) {
-          props.onChange?.(e, props.maximum || 0);
+          props.onChange?.(e, props.maximum || 0, rawValue);
         }
         if (!isNil(value) && minimumIsValid && (props.minimum || 0) > value) {
-          props.onChange?.(e, props.minimum || 0);
+          props.onChange?.(e, props.minimum || 0, rawValue);
         }
       }
 
